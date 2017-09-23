85ec7a2e81d4fbc7c1d9a8f00b86bc5c770972b0
Umer Khan <umerk123@Umers-MacBook-Pro.local>
2017-09-22 19:48:18 -0500

    Added Facebook and MapView

---
+++ 901wave/ViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  901wave
+//
+//  Created by Kareem Dasilva on 9/6/17.
+//  Copyright © 2017 Poeen. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
+import FirebaseAuth
+import SwiftKeychainWrapper
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+  
+    
+    
+    
+    @IBAction func loginWithFacebook(_ sender: Any) {
+        
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Umer: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("Umer: User canceled Facebook authentication")
+            } else {
+                print("Umer: Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+                self.getFacebookUserInfo()
+                self.performSegue(withIdentifier: "toMainFeed", sender: self)
+                
+            }
+        }
+    }
+    
+    func getFacebookUserInfo() {
+        if(FBSDKAccessToken.current() != nil)
+        {
+            //print permissions, such as public_profile
+            print(FBSDKAccessToken.current().permissions)
+            let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields" : "id, name, email"])
+            let connection = FBSDKGraphRequestConnection()
+            
+            connection.add(graphRequest, completionHandler: { (connection, result, error) -> Void in
+                
+                let data = result as! [String : AnyObject]
+                
+                //let email = data["email"] as? String
+                
+                let name = data["name"] as? String
+                
+                let FBid = data["id"] as? String
+                
+                let url = NSURL(string: "https://graph.facebook.com/\(FBid!)/picture?type=large&return_ssl_resources=1")
+                // let profilePicture = UIImage(data: NSData(contentsOf: url! as URL)! as Data)
+                
+                let userData = ["Name":name]
+                DataService.ds.createFirebaseUser(uid: FBid!, userData: userData as! Dictionary<String, String>)
+                
+            })
+            connection.start()
+        }
+    }
+    
+    
+    
+    private func completeSignIn(id: String) {
+        let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID) //.setString
+        print("umer: Data saved to keychain \(keychainResult)")
+    }
+    
+    
+    private func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Umer: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("Umer: Successfully authenticated with Firebase")
+                if let user = user{
+                    self.completeSignIn(id: user.uid)
+                }
+                
+            }
+        })
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let _ = KeychainWrapper.standard.string(forKey: KEY_UID){            print("Umer: ID found in Keychain")
+            performSegue(withIdentifier: "toMainFeed", sender: self)
+        }
+        
+    }
+
+    
+    
+    
+    
+    
+    
+
+}
+
---
+++ Podfile
@@ -0,0 +1,28 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target '901wave' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for 901wave
+pod 'Firebase/Core'
+pod 'Firebase/Database'
+pod 'Firebase/Auth'
+pod 'Firebase/Storage'
+pod 'FBSDKLoginKit'
+pod 'SwiftKeychainWrapper'
+pod 'IQKeyboardManagerSwift'
+pod 'Firebase/Messaging'
+
+  target '901waveTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target '901waveUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
