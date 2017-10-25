63e72205d98261dfd8b9b114229459b9f863e056
Kareem Dasilva <kareemdasilva@kareems-imac.memphis.edu>
2017-10-25 16:54:06 -0500

    Added Facebook to the profile view

---
+++ 901wave/UserProfileVC.swift
@@ -0,0 +1,67 @@
+//
+//  UserProfileVC.swift
+//  901wave
+//
+//  Created by Kareem Dasilva on 10/25/17.
+//  Copyright © 2017 Poeen. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class UserProfileVC: UIViewController {
+    
+    @IBOutlet weak var profilePic: UIImageView!
+    
+    @IBOutlet weak var profileName: UILabel!
+    
+    @IBOutlet weak var aboutMeLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    
+    func getFacebookUserInfo() {
+    if(FBSDKAccessToken.current() != nil)
+    {
+    //print permissions, such as public_profile
+    print("Umer The permissions are \(FBSDKAccessToken.current().permissions)")
+    let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields" : "id, name, email, about, location, gender"])
+    let connection = FBSDKGraphRequestConnection()
+    
+    connection.add(graphRequest, completionHandler: { (connection, result, error) -> Void in
+    
+    let data = result as! [String : AnyObject]
+    
+    let FBid = data["id"] as? String
+    
+    self.profileName.text = data["name"] as? String
+        
+    self.aboutMeLabel.text = data["about"] as? String
+        
+    self.cityLabel.text = data["location"] as? String
+        
+        
+    
+    let url = NSURL(string: "https://graph.facebook.com/\(FBid!)/picture?type=large&return_ssl_resources=1")
+    self.profilePic.image = UIImage(data: NSData(contentsOf: url! as URL)! as Data)
+    })
+    connection.start()
+    }
+    }
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getFacebookUserInfo()
+        profilePic.layer.borderWidth = 1
+        profilePic.layer.masksToBounds = true
+        profilePic.layer.cornerRadius = profilePic.frame.height/2
+        profilePic.clipsToBounds = true
+        profilePic.layer.borderColor = UIColor.clear.cgColor
+
+        
+        
+    }
+}
