2bf3ce6be58b13dc3a3491185f4e38f18559581e
umerk12345@hotmail.com <MajesticFlame007>
2018-05-21 19:41:08 -0500

    added files

--- 901wave/WaveGenerator.swift
+++ 901wave/WaveGenerator.swift
@@ -14,20 +14,20 @@ func generateWaveImage(count:Int) -> UIImage{
     
     switch true {
     case count < 4 :
-        return UIImage(named: "Wave - 1 Red")!
+        return UIImage(named: "Location")!
     case count > 5 && count < 10 :
-        return UIImage(named: "Wave - 1 Red")!
+        return UIImage(named: "Location")!
     case count > 11 && count < 15 :
-        return UIImage(named: "Wave - 2 Orange")!
+        return UIImage(named: "Location")!
     case count > 16 && count < 20 :
-        return UIImage(named: "Wave - 3 Yellow")!
+        return UIImage(named: "Location")!
     case count > 21 && count < 25 :
-        return UIImage(named: "Wave - 4 Green")!
+        return UIImage(named: "Location")!
     case count > 25 :
-        return UIImage(named: "Wave - 5 Blue")!
+        return UIImage(named: "Location")!
         
     default:
-        return UIImage(named: "Wave - 1 Red")!
+        return UIImage(named: "Location")!
     }
     
 }
